45b565ea72d6d5b82ae1ff1c3ef1ae3c35c41f69
Vladyslav <vladislavshvetsua@gmail.com>
2022-09-06 13:41:13 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    //Video buffer
+    static int width =17;
+    static int height =3;
+    static char[][] videoBuffer = new char[width][height];
+    //*************
+
+    //This method cleans console and prints videoBuffer
+    public static void tick(){
+        
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
+}
